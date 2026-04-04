885898a4acd5cc7d73802e486109ec914eae6320
nathanatLawdex <liszewskinathan@gmail.com>
2026-04-04 20:07:15 +1000

    Delete app/resources/[id]/page.tsx

--- app/resources/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-import ResourceDetailClient from './ResourceDetailClient';
-
-export default async function Page(props: any) {
-  const resolved = await props.params;
-  return <ResourceDetailClient id={resolved?.id ?? ''} />;
-}
